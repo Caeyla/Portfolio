a9bf92177d38b00e4a81a64f3158349a63eeebb4
Caeyla <92775744+Caeyla@users.noreply.github.com>
2024-04-22 17:02:32 +0300

    contact form

--- styles/js/index.js
+++ styles/js/index.js
@@ -1,7 +1,7 @@
-
+// import emailjs from 'emailjs-com';
 const active_class = "inline-block  text-blue-600 border-b-2 border-blue-600 rounded-t-lg  dark:text-blue-500 dark:border-blue-500";
 const inactive_class = "inline-block   rounded-t-lg hover:text-blue-400   dark:hover:text-blue-400";
-const ids = ["about", "projects", "guest-book"];
+const ids = ["about", "projects", "contact"];
 
 function changeClass(event) {
     id = event.target.id;
@@ -18,4 +18,12 @@ function changeClass(event) {
         }
     });
 }
-    
+// send email with email js
+document.getElementById("send").addEventListener("click", function () {
+    emailjs.sendForm('contact-portfolio', 'template_e5rxge1', '#myForm')
+        .then(function (response) {
+            alert("Email sent successfully");
+        }, function (error) {
+            alert("Failed to send email");
+        });
+});
