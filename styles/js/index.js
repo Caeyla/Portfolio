d1280c24b5210fc42402fef2a84496f5b6a912d9
Caeyla <92775744+Caeyla@users.noreply.github.com>
2024-04-22 13:57:16 +0300

    add update

--- styles/js/index.js
+++ styles/js/index.js
@@ -1,7 +1,7 @@
 
 const active_class = "inline-block  text-blue-600 border-b-2 border-blue-600 rounded-t-lg  dark:text-blue-500 dark:border-blue-500";
 const inactive_class = "inline-block   rounded-t-lg hover:text-blue-400   dark:hover:text-blue-400";
-const ids = ["about", "projects", "guest-book","services"];
+const ids = ["about", "projects", "guest-book"];
 
 function changeClass(event) {
     id = event.target.id;
